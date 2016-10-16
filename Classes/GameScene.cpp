0f8b21b6d8661b3634bbf13340af13bf0ce5949f
Maxim Rossiytsev <max@rossiytsev.com>
2016-10-16 16:32:12 +0300

    camera system

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -19,6 +19,38 @@ bool GameScene::init() {
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
 	this->setColor(ccc3(225, 0, 225));
+
+	auto sprite = CCSprite::create("sprites/72.png");
+	sprite->setPosition3D(Vec3(visibleSize.width/2, visibleSize.height/2, 0.0));
+
+	//this->setCameraMask((unsigned short)CameraFlag::USER2, true);
+	this->addChild(sprite);
+	/*auto camera = Camera::createPerspective(60, (float)visibleSize.width / visibleSize.height, 1.0, 1000);
+	camera->setCameraFlag(CameraFlag::USER2);
+	camera->setPosition3D(Vec3(visibleSize.width / 2, visibleSize.height / 2, 1000.0));
+	camera->lookAt(sprite->getPosition3D(), Vec3(0.0, 0.0, 0.0));
+	this->addChild(camera);
+	*/
+	auto listener = EventListenerTouchOneByOne::create();
+	listener->setSwallowTouches(true);
+	listener->setEnabled(true);
+	auto followAction = Follow::create(sprite, Rect::ZERO);
+	
+	this->runAction(followAction);
+	
+
+	listener->onTouchBegan = [=](cocos2d::Touch* touch, cocos2d::Event* event){
+		
+		auto actionMove = MoveTo::create(1, touch->getLocation());
+		sprite->runAction(actionMove);
+		
+		/*sprite->setPosition3D(Vec3(position.x+touch->getDelta().x, position.y+touch->getDelta().y, 0.0));
+		/*camera->setPosition3D(Vec3(touch->getLocation().x, touch->getLocation().y, 10.0));
+		camera->lookAt(sprite->getPosition3D(), Vec3(0.0, 0.0, 0.0));*/
+		return true;
+	};
+
+	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
 }
 
 void GameScene::menuCloseCallback(Ref* ref){
